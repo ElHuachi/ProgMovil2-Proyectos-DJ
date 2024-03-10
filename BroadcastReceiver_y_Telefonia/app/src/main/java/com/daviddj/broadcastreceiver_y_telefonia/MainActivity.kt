d98fc795f000ad8d4bc9402f9da4ccd021712fe8
David09 <s20120178@alumnos.itsur.edu.mx>
2024-03-09 22:12:38 -0600

    Solicitud del rol: ROLE_CALL_SCREENING para filtrado de llamadas

--- BroadcastReceiver_y_Telefonia/app/src/main/java/com/daviddj/broadcastreceiver_y_telefonia/MainActivity.kt
+++ BroadcastReceiver_y_Telefonia/app/src/main/java/com/daviddj/broadcastreceiver_y_telefonia/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.daviddj.broadcastreceiver_y_telefonia
 
+import android.app.role.RoleManager
+import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
@@ -15,6 +18,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.daviddj.broadcastreceiver_y_telefonia.ui.theme.BroadcastReceiver_y_TelefoniaTheme
 
 class MainActivity : ComponentActivity() {
+    private val REQUEST_CODE = 1 // Código de solicitud único
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +31,25 @@ class MainActivity : ComponentActivity() {
                 ) {
                     SmsScreen()
                 }
+            }
+        }
+        // Solicitar el rol de detección de llamadas
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q) {
+            val roleManager = getSystemService(RoleManager::class.java)
+            if (!roleManager.isRoleHeld(RoleManager.ROLE_CALL_SCREENING)) {
+                val intent = roleManager.createRequestRoleIntent(RoleManager.ROLE_CALL_SCREENING)
+                startActivityForResult(intent, REQUEST_CODE)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(applicationContext,"Permiso de intercepción concedido", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(applicationContext,"La app no funcionará por falta de permiso", Toast.LENGTH_SHORT).show()
             }
         }
     }
