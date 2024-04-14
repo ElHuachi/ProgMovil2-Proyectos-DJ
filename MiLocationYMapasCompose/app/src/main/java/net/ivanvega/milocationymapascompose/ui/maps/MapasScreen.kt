9a350642343626c61d18eb234537ae7c5a8fdc54
David09 <s20120178@alumnos.itsur.edu.mx>
2024-04-13 19:32:17 -0600

    Código de Inicio Google Maps

---
+++ MiLocationYMapasCompose/app/src/main/java/net/ivanvega/milocationymapascompose/ui/maps/MapasScreen.kt
@@ -0,0 +1,55 @@
+package net.ivanvega.milocationymapascompose.ui.maps
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.unit.dp
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.PolylineOptions
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.MapType
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerState
+import com.google.maps.android.compose.rememberCameraPositionState
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
+import com.google.android.gms.maps.GoogleMap
+import android.location.Location
+import com.google.maps.android.compose.Circle
+import com.google.maps.android.compose.Polygon
+import com.google.maps.android.compose.Polyline
+import com.google.maps.android.ktx.*
+
+@Composable
+fun MiMapa(){
+    val singapore = LatLng(1.35, 103.87)
+    val cameraPositionState = rememberCameraPositionState {
+        position = CameraPosition.fromLatLngZoom(singapore, 10f)
+    }
+    GoogleMap(
+        modifier = Modifier.fillMaxSize(),
+        cameraPositionState = cameraPositionState
+    ) {
+        Marker(
+            state = MarkerState(position = singapore),
+            title = "Singapore",
+            snippet = "Marker in Singapore"
+        )
+    }
+}
+
