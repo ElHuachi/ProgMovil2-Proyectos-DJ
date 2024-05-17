321da59d8d87dded7fd7c4f7c50f3cf927d33c88
Juan Jose AG <juanjosearreola99@gmail.com>
2024-05-16 23:16:23 -0600

    Comiit SwitftUI

--- SpaceTutorial/iosApp/iosApp/iOSApp.swift
+++ SpaceTutorial/iosApp/iosApp/iOSApp.swift
@@ -1,10 +1,21 @@
 import SwiftUI
+import Shared
 
 @main
 struct iOSApp: App {
-	var body: some Scene {
-		WindowGroup {
-			ContentView()
-		}
-	}
+    init() {
+        KoinHelperKt.doInitKoin()
+    }
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView(viewModel: .init())
+        }
+    }
+}
+
+struct iOSApp_Previews: PreviewProvider {
+    static var previews: some View {
+        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+    }
 }
